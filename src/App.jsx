5ff39585428d3082188ccc8c9872e1fca7a49f42
ShamyratAtayev <samurad1310@gmail.com>
2023-12-15 20:57:45 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+      <div className="App">
+        <div className="bg-green min-h-screen">
+          
+        </div>
+      </div>
+  );
+}
+
+export default App
